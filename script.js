ef7a72277e9c943f75cb6523c0c2ad52dc0dc174
Ronint-chan <nut.tara@mail.kmutt.ac.th>
2025-11-03 04:03:52 +0700

    03/11 fix
    เพิ่มหน้า shop กับ product detail

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ if (close) {
     nav.classList.remove('active');
   })
 }
+
+// Product Details
+
+var MainImg = document.getElementById("MainImg");
+var smalling = document.getElementById("small-img");
+
+smalling[0].onclick = function(){
+  MainImg.src = smalling[0].src;
+}
+smalling[1].onclick = function(){
+  MainImg.src = smalling[1].src;
+}
+smalling[2].onclick = function(){
+  MainImg.src = smalling[2].src;
+}
+smalling[3].onclick = function(){
+  MainImg.src = smalling[3].src;
+}
